514ba5accf71210cb642938cc50d81167bc07eb7
sgtWalker <rzrsdiego@outlook.com>
2021-07-25 19:38:46 -0300

    Conclusão do desenvolvimento. Sem aplicar correção comparando com o código do prof Nelio

--- ExercicioFixacao199CSharpNelioAlves/Services/IOnlinePaymentService.cs
+++ ExercicioFixacao199CSharpNelioAlves/Services/IOnlinePaymentService.cs
@@ -3,7 +3,7 @@ namespace ExercicioFixacao199CSharpNelioAlves.Services
 {
     interface IOnlinePaymentService
     {
-        public double PaymentFee(double amount);
-        public double Interest(double amount, int month);
+        double PaymentFee(double amount);
+        double Interest(double amount, int month);
     }
 }
